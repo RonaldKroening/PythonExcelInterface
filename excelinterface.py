18fa140f31bf9a8bdc111daafcbe25e776710d1b
Ronald Kroening <30726351+RonaldKroening@users.noreply.github.com>
2021-09-13 19:49:26 -0400

    Update excelinterface.py

--- excelinterface.py
+++ excelinterface.py
@@ -4,7 +4,7 @@ workbook = load_workbook(filename="YOUR FILENAME HERE .xlsx")
 sheet = workbook.active
 
 def make_search(term):
-    s = "https://www.google.com/search?q="+term+"&sxsrf=AOaemvJvhzbx5Thy2rW00q3qiD4tFIORzA%3A1631560943795&source=hp&ei=76Q_Yc2HLsbVrtoP3J6hyAE&iflsig=ALs-wAMAAAAAYT-y__6YKdjWByfSl5qtNTMAmPU2qb-V&oq=hi&gs_lcp=Cgdnd3Mtd2l6EAMyBAgjECcyBQgAEJECMgQIABBDMgUIABCRAjIHCC4QsQMQQzIECC4QQzIECAAQQzIECAAQQzIECC4QQzINCC4QsQMQxwEQ0QMQQzoHCCMQ6gIQJzoOCC4QgAQQsQMQxwEQowI6CwgAEIAEELEDEIMBOggIABCABBCxA1C9KlieLGCnL2gBcAB4AIABaogB0wGSAQMwLjKYAQCgAQGwAQo&sclient=gws-wiz&ved=0ahUKEwiNu-2T1vzyAhXGqksFHVxPCBkQ4dUDCAk&uact=5"
+    s = "https://www.google.com/search?q="+term
     webbrowser.open(s)
 def column_search(head,i,column):
     cell1 = head+str(i)
